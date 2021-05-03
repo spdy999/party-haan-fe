12c6292fd43b5a6e92cabb932399df543d38e74d
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-04 05:49:45 +0700

    tested formik-material-ui

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,70 +1,64 @@
-import Layout from '../../components/layout';
-import * as Yup from 'yup';
-import { withFormik, FormikProps, FormikErrors, Form, Field } from 'formik';
-interface FormValues {
+import { Formik, Form, Field } from 'formik';
+import { Button, LinearProgress } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+
+interface Values {
   email: string;
   password: string;
 }
-interface OtherProps {
-  message: string;
-}
-
-const isValidEmail = (email: string): boolean => true;
-// Aside: You may see InjectedFormikProps<OtherProps, FormValues> instead of what comes below in older code.. InjectedFormikProps was artifact of when Formik only exported a HoC. It is also less flexible as it MUST wrap all props (it passes them through).
-const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
-  const { touched, errors, isSubmitting, message } = props;
-  return (
-    <Form>
-      <h1>{message}</h1>
-      <Field type="email" name="email" />
-      {touched.email && errors.email && <div>{errors.email}</div>}
-
-      <Field type="password" name="password" />
-      {touched.password && errors.password && <div>{errors.password}</div>}
-
-      <button type="submit" disabled={isSubmitting}>
-        Submit
-      </button>
-    </Form>
-  );
-};
-
-// The type of props MyForm receives
-interface MyFormProps {
-  initialEmail?: string;
-  message: string; // if this passed all the way through you might do this or make a union type
-}
-
-// Wrap our form with the withFormik HoC
-const MyForm = withFormik<MyFormProps, FormValues>({
-  // Transform outer props into form values
-  mapPropsToValues: (props) => {
-    return {
-      email: props.initialEmail || '',
-      password: '',
-    };
-  },
-
-  // Add a custom validation function (this can be async too!)
-  validate: (values: FormValues) => {
-    const errors: FormikErrors<FormValues> = {};
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!isValidEmail(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-    return errors;
-  },
-
-  handleSubmit: (values) => {
-    // do submitting things
-  },
-})(InnerForm);
 
 export default function Login() {
   return (
-    <Layout>
-      <MyForm message="Sign up" />
-    </Layout>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validate={(values) => {
+        const errors: Partial<Values> = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          setSubmitting(false);
+          alert(JSON.stringify(values, null, 2));
+        }, 500);
+      }}
+    >
+      {({ submitForm, isSubmitting }) => (
+        <Form>
+          <Field
+            component={TextField}
+            name="email"
+            type="email"
+            label="Email"
+          />
+          <br />
+          <Field
+            component={TextField}
+            type="password"
+            label="Password"
+            name="password"
+          />
+          {isSubmitting && <LinearProgress />}
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            onClick={submitForm}
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
