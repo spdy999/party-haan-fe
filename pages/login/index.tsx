30814de3fcb3575411158ba779ea471e9edb5ce8
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-04 05:58:53 +0700

    added layout to login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,6 +1,7 @@
 import { Formik, Form, Field } from 'formik';
 import { Button, LinearProgress } from '@material-ui/core';
 import { TextField } from 'formik-material-ui';
+import Layout from '../../components/layout';
 
 interface Values {
   email: string;
@@ -9,56 +10,62 @@ interface Values {
 
 export default function Login() {
   return (
-    <Formik
-      initialValues={{
-        email: '',
-        password: '',
-      }}
-      validate={(values) => {
-        const errors: Partial<Values> = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          alert(JSON.stringify(values, null, 2));
-        }, 500);
-      }}
-    >
-      {({ submitForm, isSubmitting }) => (
-        <Form>
-          <Field
-            component={TextField}
-            name="email"
-            type="email"
-            label="Email"
-          />
-          <br />
-          <Field
-            component={TextField}
-            type="password"
-            label="Password"
-            name="password"
-          />
-          {isSubmitting && <LinearProgress />}
-          <br />
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={isSubmitting}
-            onClick={submitForm}
-          >
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
+    <Layout>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validate={(values) => {
+          const errors: Partial<Values> = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+            alert(JSON.stringify(values, null, 2));
+          }, 500);
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <Field
+              component={TextField}
+              name="email"
+              type="email"
+              label="อีเมล"
+            />
+            <br />
+            <Field
+              component={TextField}
+              type="password"
+              label="รหัสผ่าน"
+              name="password"
+            />
+            {isSubmitting && <LinearProgress />}
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={isSubmitting}
+              onClick={submitForm}
+            >
+              เข้าสู่ระบบ
+            </Button>
+            <br />
+            <Button variant="contained" color="primary">
+              สร้างบัญชีผู้ใช้
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
   );
 }
