d2feaf557a046623ca6acc64ef116513d0f7ffb7
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-04 07:04:34 +0700

    style central layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,21 @@
-import { Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Container>{children}</Container>;
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    margin: 0,
+    padding: '70px 50px',
+  },
+});
+export default function Layout(props: { children: React.ReactNode }) {
+  const classes = useStyles(props);
+
+  return (
+    <div className={classes.root}>
+      <Grid item style={{ textAlign: 'center' }}>
+        {props.children}
+      </Grid>
+    </div>
+  );
 }
