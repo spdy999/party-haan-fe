74cbcc08e09fe5b4718562c0842cdd121190c71e
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-05 06:34:24 +0700

    added get parties data from api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { AppWrapper } from '../context/state';
 import { LoginContextWrapper } from '../context/login/LoginProvider';
+import { PartyContextWrapper } from '../context/party/PartyProvider';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
       <LoginContextWrapper>
-        <Component {...pageProps} />
+        <PartyContextWrapper>
+          <Component {...pageProps} />
+        </PartyContextWrapper>
       </LoginContextWrapper>
     </AppWrapper>
   );
--- pages/party/index.tsx
+++ pages/party/index.tsx
@@ -1,3 +1,11 @@
+import { usePartyContext } from '../../context/party/PartyProvider';
 export default function Party() {
-  return <h1>Party</h1>;
+  const { partyMessage, parties } = usePartyContext();
+
+  return (
+    <div>
+      <h1>{partyMessage}</h1>
+      <h1>{JSON.stringify(parties)}</h1>
+    </div>
+  );
 }
